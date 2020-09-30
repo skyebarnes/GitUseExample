d0b3d907e7df8266244996fd21ecc69a69e0c0de
Skye Barnes <skyebarnes285@gmail.com>
2020-09-30 11:28:21 -0500

    Add new map for itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Barnes, Skye A on 9/30/20.
+//  Copyright © 2020 Barnes, Skye A. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
         print("This is a Git tutorial")
     }
 
+    
 }
 
 
